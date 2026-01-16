463e68b6bc44ec914cd87e3bef75f37fb026be8c
austinaiprofitgen <austinaiprofitgen@gmail.com>
2026-01-17 01:05:33 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-experimental: {
-turbo: false
-}
+  webpack(config) {
+    return config;
+  }
 };
 
 export default nextConfig;
